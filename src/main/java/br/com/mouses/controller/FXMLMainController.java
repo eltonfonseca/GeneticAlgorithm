4231acda5121866bc7761f3c282cf94c6ae0cc69
Elton <Elton@DESKTOP-1550RBB>
2018-10-28 16:24:44 -0300

    Rank e Rank + Torneio

--- src/main/java/br/com/mouses/controller/FXMLMainController.java
+++ src/main/java/br/com/mouses/controller/FXMLMainController.java
@@ -8,11 +8,17 @@ package br.com.mouses.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import java.awt.Component;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -39,32 +45,34 @@ public class FXMLMainController implements Initializable {
     private JFXComboBox<String> selecao_box;
     @FXML
     private JFXComboBox<Integer> geracoes_box;
+    @FXML
+    private BorderPane borderPane;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
         pop_box.getItems().addAll(10, 100, 500, 1000, 5000);
         pop_box.setValue(10);
-        
+
         cruz_box.getItems().addAll(80, 85, 90);
         cruz_box.setValue(80);
-        
+
         mut_box.getItems().addAll(1, 3, 5);
         mut_box.setValue(1);
-        
+
         selecao_box.getItems().addAll("Rank", "Rank + Torneio");
         selecao_box.setValue("Rank");
-        
+
         geracoes_box.getItems().addAll(100, 250, 500, 1000, 5000);
         geracoes_box.setValue(100);
     }
 
     @FXML
     private void evoluir(ActionEvent event) {
-                
+
         int tamanho = Integer.parseInt(tam_field.getText());
         int origem = Integer.parseInt(origem_field.getText());
         int destino = Integer.parseInt(destino_field.getText());
@@ -73,13 +81,19 @@ public class FXMLMainController implements Initializable {
         int mutacao = mut_box.getSelectionModel().getSelectedItem();
         String selecao = selecao_box.getSelectionModel().getSelectedItem();
         int geracoes = geracoes_box.getSelectionModel().getSelectedItem();
-                
+
         ConfigurationController config = new ConfigurationController(tamanho, origem, destino, populacao, cruzamento, mutacao, selecao, geracoes);
         GeneticAlgorithm ga = new GeneticAlgorithm(config.getConfig());
-        
+
         System.out.println("Gerando população inicial...");
         ga.populacaoInicial();
-        ga.showPopulation();
+        ga.showPopulation(ga.getPopulacao());
         ga.seleciona();
+        ga.showPopulation(ga.getNovaPopulacao());
+
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Informação");
+        alert.setHeaderText("Evoluído com Sucesso!");
+        alert.showAndWait();
     }
 }
--- src/main/java/br/com/mouses/controller/GeneticAlgorithm.java
+++ src/main/java/br/com/mouses/controller/GeneticAlgorithm.java
@@ -56,7 +56,7 @@ public class GeneticAlgorithm {
             cromossomo.setFitness(calcularFitness(cromossomo.getGenes()));
             populacao.add(cromossomo);
         }
-        ordenaPopulacaoPorFitness();
+        ordenaPopulacaoPorFitness(false);
     }
     
     public void seleciona() {
@@ -64,21 +64,57 @@ public class GeneticAlgorithm {
         int tamanho_nova_populacao = this.config.getTax_cruz() * this.populacao.size() / 100;
                 
         if(this.config.getSelecao().equals("Rank")) {
-            
-        } 
+            for(int i = 0; i < tamanho_nova_populacao; i++) {
+                this.nova_populacao.add(this.populacao.get(i));
+            }
+        }
         else {
-            System.out.println("Rank + Torneio");
+            int elitismo = tamanho_nova_populacao * 10 / 100;
+            
+            for(int i = 0; i < elitismo; i++) {
+                this.nova_populacao.add(this.populacao.get(i));
+                System.out.println("Adicionando por Rank");
+            }
+            
+            System.out.println("Agora adicionando de forma randomica");
+            
+            while(this.nova_populacao.size() != tamanho_nova_populacao) {
+                rand = new Random();
+                int randA = (int) rand.nextInt(this.populacao.size());
+                int randB = (int) rand.nextInt(this.populacao.size());
+                Cromossomo a = this.populacao.get(randA);
+                Cromossomo b = this.populacao.get(randB);
+                
+                if(a.getFitness() <= b.getFitness())
+                    this.nova_populacao.add(a);
+                else
+                    this.nova_populacao.add(b);
+            }
+            
+            ordenaPopulacaoPorFitness(true);
         }
     }
     
-    public void showPopulation() {
-        for(int i = 0; i < this.populacao.size(); i++) {
+    public void showPopulation(List<Cromossomo> populacao) {
+        System.out.println();
+        for(int i = 0; i < populacao.size(); i++) {
             System.out.print((i + 1) + " Fitness: " + populacao.get(i).getFitness() + " Gene: ");
             populacao.get(i).show_genes();
         }
     }
     
-    private void ordenaPopulacaoPorFitness() {
-        this.populacao.sort((Cromossomo a, Cromossomo b) -> a.getFitness() - b.getFitness());
-    } 
+    private void ordenaPopulacaoPorFitness(boolean ordenaNovaPopulacao) {
+        if(ordenaNovaPopulacao)
+            this.nova_populacao.sort((Cromossomo a, Cromossomo b) -> a.getFitness() - b.getFitness());
+        else
+            this.populacao.sort((Cromossomo a, Cromossomo b) -> a.getFitness() - b.getFitness());
+    }
+    
+    public List<Cromossomo> getPopulacao() {
+        return this.populacao;
+    }
+    
+    public List<Cromossomo> getNovaPopulacao() {
+        return this.nova_populacao;
+    }
 }
